cefdcfe7eb941410137edb6298987dc67f01da09
Hamdy <>
2025-08-28 04:14:13 +0300

    feat: Update app version to 1.1.0 and adjust iOS version parsing
    The application version has been updated to `1.1.0+1` in `pubspec.yaml`.
    
    Additionally, the `ChangeAppVersionUseCase` has been modified to handle iOS version parsing differently. For iOS, it now extracts only the major and minor version components (e.g., "1.1" from "1.1.0") before attempting to parse it as a double. This change does not affect Android version parsing.
    
    The iOS project file (`project.pbxproj`) also includes a new build phase `[CP] Copy Pods Resources`.

--- lib/domain/usecases/change_app_version_use_case.dart
+++ lib/domain/usecases/change_app_version_use_case.dart
@@ -34,6 +34,11 @@ class ChangeAppVersionUseCase extends BaseUseCase<CheckAppVersionModel>{
     String versionName = info.version;      // e.g. "1.2.3"
     String buildNumber = info.buildNumber;  // e.g. "42"
     print("App Version: $versionName ($buildNumber)");
+
+    if(Platform.isIOS){
+      String majorMinor = versionName.split(".").take(2).join(".");
+      return double.tryParse(majorMinor);
+    }
     return double.tryParse(versionName);
   }
 
