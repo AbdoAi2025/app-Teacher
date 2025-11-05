642df06d7812e1a58579886b6c7bb1458724953c
Hamdy <>
2025-11-05 12:18:59 +0200

    refactor: Abstract AddStudentUseCase from BaseUseCase
    This commit refactors `AddStudentUseCase` to extend a new `BaseUseCase`.
    
    The primary change involves moving the `try-catch` block logic into a shared `call` method within the `BaseUseCase`, simplifying the `execute` method in `AddStudentUseCase`.

--- lib/domain/usecases/add_student_use_case.dart
+++ lib/domain/usecases/add_student_use_case.dart
@@ -1,21 +1,20 @@
 import 'package:teacher_app/base/AppResult.dart';
 import 'package:teacher_app/data/repositories/students_repository.dart';
+import 'package:teacher_app/domain/base_use_case.dart';
 import 'package:teacher_app/requests/add_student_request.dart';
 
 import '../../data/responses/add_student_response.dart';
 import '../events/students_events.dart';
 
-class AddStudentUseCase {
+class AddStudentUseCase extends BaseUseCase<AddStudentResponse?> {
 
   StudentsRepository repository = StudentsRepository();
 
   Future<AppResult<AddStudentResponse?>> execute(AddStudentRequest request) async {
-    try{
-       var items =  await repository.addStudent(request);
-       StudentsEvents.onStudentAdded();
-       return AppResult.success(items);
-    }on Exception catch(ex){
-      return AppResult.error(ex);
-    }
+   return call(() async {
+      var items =  await repository.addStudent(request);
+      StudentsEvents.onStudentAdded();
+      return AppResult.success(items);
+    });
   }
 }
